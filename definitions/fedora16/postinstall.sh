1dbe5dba8839887d709947c7edfc475a4b57dfda
jtimberman <joshua@opscode.com>
2012-01-09 23:29:54 -0700

    update definitions for newer releases

---
+++ definitions/fedora16/postinstall.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+date > /etc/vagrant_box_build_time
+
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+
+yum -y update
+
+yum -y install curl wget
+
+cd /tmp
+wget http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso
+mount -o loop,ro VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+rm VBoxGuestAdditions_$VBOX_VERSION.iso
+
+curl -L http://opscode/chef/install.sh | bash
+
+exit
+
+# EOF
